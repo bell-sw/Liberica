79272428ea3809814bc69045f1f977d4d0580dca
Mikhail Chernov <mikhail.chernov@bell-sw.com>
2019-10-29 12:59:29 +0300

    Added old version 10, 9, 12, etc.

--- docker/repos/liberica-openjdk-alpine/old/build_all.sh
+++ docker/repos/liberica-openjdk-alpine/old/build_all.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-for f in `find . -name "Dockerfile"`
+for f in `find . -name "Dockerfile" | grep -vE "9.0.4|9.0.1|10.0.0|10.0.1" `
 do
   DIR=`dirname ${f}`
+  echo ${DIR}
   cd ${DIR}
   > log
   docker build . 2>&1  > log || { cat log; echo $f; exit 1; }
 
   id=`grep "Successfully built" log | cut -f 3 -d \ `
   docker run $id java -version
+  echo "-----"
   rm -f log
   cd -
   # exit 0
